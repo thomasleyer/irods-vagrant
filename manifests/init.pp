0806948e8f431a010e4bc634c3c1745273e1ec77
Thomas Leyer <selecticon@googlemail.com>
2015-02-24 07:16:58 +0100

    use postgresql::server::database and no dependencies

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,21 +1,34 @@
 node default {
-  class { 'postgresql::globals':
-    #manage_package_repo => true,
-    #version             => '9.3',
-  } ->
   class { 'postgresql::server':
     listen_addresses => '*',
-  } ->
-  postgresql::server::db { 'ICAT':
-    user     => 'irods',
-    password => postgresql_password('irods', 'password'),
-    require  => Class['postgresql::server'],
-  } -> 
-  postgresql::server::role { 'myirods':
-    db => 'ICAT',
-    login => true,
-    superuser => true, 
   } 
 
-}
+  postgresql::server::database { 'ICAT':
+    owner     => 'irods',
+    encoding => $postgresql::server::encoding,
+    template => 'template0',
+    locale   => $postgresql::server::locale,
+    istemplate => false,
+    require    => [Class['postgresql::server'],Postgresql::Server::Role['irods']],
+  } 
+  postgresql::server::role { 'myirods':
+    db            => 'ICAT',
+    login         => true,
+    superuser     => true, 
+    createdb      => true,
+    password_hash => postgresql_password('myirods', 'password'),
+  }   
+  postgresql::server::role { 'irods':
+    login         => true,
+    createdb      => true,
+    superuser     => true,
+    password_hash => postgresql_password('irods', 'password'),
+  }
+#  postgresql::server::database_grant { "GRANT irods - ALL - ICAT":
+#    privilege => 'ALL',
+#    db        => 'ICAT',
+#    role      => 'irods',
+#  } -> Postgresql::Validate_db_connection<| database_name == 'ICAT' |>
 
+
+}
